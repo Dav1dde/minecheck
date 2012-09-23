f8f157c05fb47c06a871082baa9b99257e017c94
David <admin@dav1d.de>
2012-09-23 19:17:56 +0200

    added gunicorn config

---
+++ gunicorn.config.py
@@ -0,0 +1,11 @@
+import os.path
+
+base_path = os.path.split(os.path.abspath(__file__))[0]
+
+bind = '0.0.0.0:9600'
+workers = 1
+keepalive = 1
+#user = 'minestatus'
+errorlog = os.path.join(base_path, 'log/error.log')
+loglevel = 'warning'
+proc_name = 'minestatus gunicorn'
